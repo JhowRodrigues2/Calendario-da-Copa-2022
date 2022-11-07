c4cc531f4178fda75ffcea8363e255e7c935e311
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-11-07 10:54:37 -0300

    atualization commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+
 const Card = ({ date, day, children }) => {
   return (
     <div className="card">
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Game = ({ player1, player2, hour }) => {
   return (
     <li>
-      <img src={`/src/assets/${player1}.svg`} alt="flag Brazil " />
+      <img src={`/src/assets/${player1}.svg`} alt={`flag ${player1}`} />
       <strong>{hour}</strong>
-      <img src={`/src/assets/${player2}.svg`} alt="flag Serbia" />
+      <img src={`/src/assets/${player2}.svg`} alt={`flag ${player2}`} />
     </li>
   );
 };
