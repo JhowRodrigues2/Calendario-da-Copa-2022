e1adbe5b3af964489f1e6461ca41ab8f701628ab
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-12-13 09:06:40 -0300

    Semi-final

--- src/Components/Semi.jsx
+++ src/Components/Semi.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Card from "./Card";
 import Game from "./Game";
-import Undefined from "/src/assets/icon-undefined.svg";
+import Argentina from "/src/assets/Argentina.svg";
+import Croatia from "/src/assets/Netherlands.svg";
+import Morocco from "/src/assets/Morocco.svg";
+import France from "/src/assets/France.svg";
 
 const Semi = () => {
   return (
@@ -13,10 +16,10 @@ const Semi = () => {
       </Helmet>
       <main className="cards">
         <Card date={"13/12"} day={"Terça"}>
-          <Game player1={Undefined} player2={Undefined} hour={"16:00"} />
+          <Game player1={Argentina} player2={Croatia} hour={"16:00"} />
         </Card>
         <Card date={"13/12"} day={"Quarta"}>
-          <Game player1={Undefined} player2={Undefined} hour={"16:00"} />
+          <Game player1={France} player2={Morocco} hour={"16:00"} />
         </Card>
       </main>
     </div>
